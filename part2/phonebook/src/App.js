a1b58c70baf35d1e722db4d3930e93fbd9873bfa
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-06-06 12:55:28 +0200

    Finish exercise 2.15

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -86,9 +86,12 @@ const App = () => {
             number: newNumber,
             id: persons.length + 1,
         };
-        setPersons(persons.concat(person));
-        setNewName("");
-        setNewNumber("");
+        axios.post("http://localhost:3001/persons", person).then((response) => {
+            console.log("response", response);
+            setPersons(persons.concat(response.data));
+            setNewName("");
+            setNewNumber("");
+        });
     };
 
     const handleNameChange = (event) => {
